890a103a01c27d117aa318a85d3410c77618bb80
Dania Alaaeldin <d.a.mahdaly@outlook.com>
2022-03-15 03:51:14 +0200

    Dialog controller base

---
+++ Assets/Scripts/Controllers/DialogController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogController : MonoBehaviour
+{
+    [SerializeField]
+    DialogView view;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
