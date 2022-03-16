dc426eee49e52f3942bf1bc9b917d8a5346b3cbe
Dania Alaaeldin <d.a.mahdaly@outlook.com>
2022-03-16 14:57:41 +0200

    Updating example scene and adding sprites

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SceneController : MonoBehaviour
 {
     public DialogOpener dialogOpener;
+    public TMP_Text hintText;
+
+    bool dialogStarted = false;
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (dialogOpener != null)
+            if (dialogOpener != null && !dialogStarted)
             {
                 dialogOpener.startDialog();
+                dialogStarted = true;
+                hintText.gameObject.SetActive(false);
             }
         }
     }
