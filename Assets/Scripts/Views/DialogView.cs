15ee2dbad49b0b12dcb82934bcb666b1990be641
Dania Alaaeldin <d.a.mahdaly@outlook.com>
2022-03-16 14:13:44 +0200

    Updating invoking option clicking in dialog view

--- Assets/Scripts/Views/DialogView.cs
+++ Assets/Scripts/Views/DialogView.cs
@@ -17,27 +17,28 @@ public class DialogView : MonoBehaviour
     [SerializeField]
     Button option3;
 
-    void initializeDialog(string name, string text, string option1Text, string option2Text, string option3Text,
-        UnityEngine.Events.UnityAction option1Event, UnityEngine.Events.UnityAction option2Event, UnityEngine.Events.UnityAction option3Event)
+    public void initializeDialog(string name, string text, string option1Text, string option2Text, string option3Text, DialogController.goNextInvoker optionEvent,
+                                 int option1ID, int option2ID, int option3ID)
     {
         this.name.text = name;
         this.text.text = text;
 
-        initializeOptionButton(option1, option1Text, option1Event);
-        initializeOptionButton(option2, option2Text, option2Event);
-        initializeOptionButton(option3, option3Text, option3Event);
+        initializeOptionButton(option1, option1Text, optionEvent, option1ID);
+        initializeOptionButton(option2, option2Text, optionEvent, option2ID);
+        initializeOptionButton(option3, option3Text, optionEvent, option3ID);
     }
 
-    void initializeOptionButton(Button optionButton, string optionText, UnityEngine.Events.UnityAction optionEvent)
+    void initializeOptionButton(Button optionButton, string optionText, DialogController.goNextInvoker optionEvent, int id)
     {
+        optionButton.onClick.RemoveAllListeners();
+        optionButton.gameObject.transform.Find("OptionText").GetComponent<TMP_Text>().text = "";
+        optionButton.enabled = false;
+
         if (!string.IsNullOrEmpty(optionText))
         {
+            optionButton.enabled = true;
             optionButton.gameObject.transform.Find("OptionText").GetComponent<TMP_Text>().text = optionText;
-            optionButton.onClick.AddListener(optionEvent);
-        }
-        else
-        {
-            optionButton.enabled = false;
+            optionButton.onClick.AddListener(delegate { optionEvent.Invoke(id); });
         }
     }
 }
